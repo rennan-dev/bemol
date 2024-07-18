ed0a2d84a20c8f381125d128bf1122b11c93b75c
Rennan-Dev <rennandev7@gmail.com>
2024-07-18 11:53:29 -0400

    Semana 2

---
+++ Semana 2/Desafio02/Desafio02/Produto.cs
@@ -0,0 +1,22 @@
+﻿class Produto {
+    public string Nome;
+    public string Descricao;
+    public float PrecoUnitario;
+    public int Quantidade;
+
+    public Produto(string nome, string descricao, float precoUnitario, int quantidade) {
+        Nome = nome;
+        Descricao = descricao;
+        PrecoUnitario = precoUnitario;
+        Quantidade = quantidade;
+    }
+
+    public void VisualizaProduto() {
+        Console.WriteLine("\n----------------------------------------");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Descrição do produto: {Descricao}");
+        Console.WriteLine($"Preço unitário: {PrecoUnitario:F2} R$");
+        Console.WriteLine($"Quantidade no estoque: {Quantidade}\n");    
+        Console.WriteLine("----------------------------------------");
+    }
+}
---
+++ Semana 2/Desafio02/Desafio02/Program.cs
@@ -0,0 +1,36 @@
+﻿//Produto prod1 = new Produto("Bolacha", "Alimentício", 3.5f, 100);
+//Produto prod2 = new Produto("Macarrão", "Alimentício", 8.99f, 40);
+//Produto prod3 = new Produto("Picanha", "Alimentício", 40f, 30);
+
+//Console.Write("Digite o nome do 4° produto: ");
+//string nomeProduto = Console.ReadLine()!;
+//Produto prod4 = new Produto(nomeProduto, "Limpeza", 40f, 30);
+
+//prod1.VisualizaProduto();
+//prod2.VisualizaProduto();
+//prod3.VisualizaProduto();
+//prod4.VisualizaProduto();
+
+List<Produto> produtos = new();
+
+while(true) {
+    Console.WriteLine("\n---------------------------------------");
+    Console.WriteLine("1 para adicionar produto");
+    Console.WriteLine("2 para visualizar produtos");
+    Console.WriteLine("0 para sair");
+    Console.Write("Digite sua resposta: ");
+    int aux = Console.Read();
+
+    switch(aux) {
+        case 1: AdicionaProd(produtos); break;
+
+        default: Console.WriteLine("Digite uma opção válida."); break;
+    }
+}
+
+void AdicionaProd(List<Produto> prods) {
+    string nomeProduto = Console.ReadLine()!;
+    if(!string.IsNullOrWhiteSpace(nomeProduto)) {
+
+    }
+}
