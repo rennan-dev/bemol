01f5e2450141964971c47cd58ab8a0dc323ee509
Rennan <144573309+rennan-dev@users.noreply.github.com>
2024-10-07 09:05:12 -0400

    Delete ComexAPI directory

--- ComexAPI/Models/Admin.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Identity;
-
-namespace WebApi.Models;
-public class Admin : IdentityUser {
-    
-    public Admin() : base() { }
-}
