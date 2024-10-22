82076aed0beeb08ea71fdcf2e4f0ea07fc1f1a1c
Rennan-Dev <rennandev7@gmail.com>
2024-10-22 11:55:06 -0400

    Remove ComexAPI e arquivos associados

--- ComexAPI/Data/Dtos/Admin/LoginAdminDto.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace WebApi.Data.Dtos;
-public class LoginAdminDto {
-
-    [Required] public string Username { get; set; }
-    [Required] public string Password { get; set; }
-}
